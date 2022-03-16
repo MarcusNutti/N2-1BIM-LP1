1a8a485337c1efddc07c6a22a3c690ea9c551c14
Marcus Nutti <marcus.nutti@nescara.com>
2022-03-15 23:54:59 -0300

    Adicionar arquivos de projeto.

---
+++ N2 1BIM LP1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace N2_1BIM_LP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
