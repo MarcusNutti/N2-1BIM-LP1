f0ec92c490b0241464af0a9e9cc6b11f9f3742c4
AlexsanderVilaca <irmaosaavr@hotmail.com>
2022-03-22 15:08:21 -0300

    ADD: Faz o CRUD
    FIX: Agora consegue gravar as experiências no banco
    
    INC: Banco alterado
    
    INC: Removidas validação do CPF

--- N2 1BIM LP1/Controllers/CurriculoController.cs
+++ N2 1BIM LP1/Controllers/CurriculoController.cs
@@ -16,7 +16,9 @@ namespace N2_1BIM_LP1.Controllers
             {
                 CurriculoDAO dao = new CurriculoDAO();
                 var lista = dao.ListaCurriculos();
-                return View(lista);
+                if (lista == null) /* se o banco estiver vazio, direciona para a tela de cadastro */
+                    return View("FormCadastro");
+                return View(lista); /* senão retorna a lista de CV */
             }
             catch (Exception er)
             {
@@ -36,9 +38,68 @@ namespace N2_1BIM_LP1.Controllers
         {
             try
             {
-                CurriculoDAO dao = new CurriculoDAO ();
+                CurriculoDAO dao = new CurriculoDAO();
                 dao.Inserir(cv);
+                /* VerificaCPF(cv); */ /* comentado para demonstração ficar mais simples */
                 return RedirectToAction("FormCadastro");
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.Message));
+            }
+        }
+        private void VerificaCPF (CurriculoViewModel cv)
+        {
+            if (!int.TryParse (cv.Cpf, out int result))
+                throw new Exception("Insira apenas dígitos.");
+            else
+            if (cv.Cpf.Length != 11)
+                throw new Exception("O número do CPF deve ter 11 dígitos.");
+        }
+        public IActionResult Alterar (string valorCPF)
+        {
+            try
+            {
+                CurriculoDAO dao = new CurriculoDAO();
+                var lista = dao.ListaCurriculos();
+
+                CurriculoViewModel umItem = new CurriculoViewModel();
+                foreach (CurriculoViewModel item in lista)
+                {
+                    if (item.Cpf == valorCPF)
+                    {
+                        umItem = item;
+                        break;
+                    }
+                }
+
+                return View("FormAlterar", umItem);
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.Message));
+            }
+        }
+        public IActionResult ConfirmarAlteracao(CurriculoViewModel cv)
+        {
+            try
+            {
+                CurriculoDAO dao = new CurriculoDAO();
+                dao.Alterar(cv);
+                return RedirectToAction("FormCadastro");
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.Message));
+            }
+        }
+        public IActionResult Excluir (string valorCPF)
+        {
+            try
+            {
+                CurriculoDAO dao = new CurriculoDAO();
+                dao.Excluir(valorCPF);
+                return RedirectToAction("Index");
             }
             catch (Exception erro)
             {
--- N2 1BIM LP1/wwwroot/js/site.js
+++ N2 1BIM LP1/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$("#table tr").click(function () {
+    $(this).addClass('selected').siblings().removeClass('selected');
+    var value = $(this).find('td:first').html();
+    //alert(value);
+    document.getElementById("txtDataSwapA").value = value; /* passa do JS para uma textbox, para dela passar para o C# */
+    document.getElementById("txtDataSwapB").value = value; /* passa do JS para uma textbox, para dela passar para o C# */
+});
 
-// Write your JavaScript code.
+$('.ok').on('click', function (e) {
+    alert($("#table tr.selected td:first").html());
+});
